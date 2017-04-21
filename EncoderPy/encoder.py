3e6e865422f77d28824130293d43b6b86eed9e3f
Joe Mathai <joemathai16@gmail.com>
2017-04-20 20:42:28 -0700

    dct added

--- EncoderPy/encoder.py
+++ EncoderPy/encoder.py
@@ -1,6 +1,5 @@
 import argparse
 import os
-
 from PIL import Image
 
 from image_dct import *
@@ -21,31 +20,60 @@ def main():
         default='/Users/jmathai/multimedia_data/output',
         help="Directory to write results to"
     )
+
     w = 960
     h = 540
     channels = 3
+
     args = parser.parse_args()
     assert os.path.exists(args.inp), "Input dir not valid."
     if not os.path.exists(args.out): os.makedirs(args.out)
     params['input'] = args.inp
     params['output'] = args.out
 
-    pixels = np.fromfile(params["input"], dtype=np.int8)
+    pixels = np.fromfile(params["input"], dtype=np.uint8)
 
     ind = 0
-    count = 0
-    while ind < len(pixels) and count < 40:
-        count += 1
-        img_array = np.zeros(shape=(h, w, channels), dtype=np.int8)
-        for y in range(h):
+    frame = 0
+    while ind < len(pixels):
+
+        img = np.zeros(shape=(h + 4, w, channels), dtype=np.uint8)
+        img_array_r = np.zeros(shape=(h + 4, w), dtype=np.uint8)
+        img_array_g = np.zeros(shape=(h + 4, w), dtype=np.uint8)
+        img_array_b = np.zeros(shape=(h + 4, w), dtype=np.uint8)
+
+        for y in range(h + 4):
             for x in range(w):
-                r = pixels[ind]
-                g = pixels[ind + w * h]
-                b = pixels[ind + w * h * 2]
-                img_array[y][x] = np.array([r, g, b])
+                if y >= h:
+                    r, g, b = 0, 0, 0
+                else:
+                    r, g, b = pixels[ind], pixels[ind + w * h], pixels[ind + w * h * 2]
+                img[y][x] = np.array([r, g, b])
+                img_array_r[y][x] = r
+                img_array_g[y][x] = g
+                img_array_b[y][x] = b
                 ind += 1
+
         ind = ind + h * w * 2
-        Image.fromarray(img_array, mode="RGB").save('{}/{}.jpg'.format(params["output"], count))
+        #Image.fromarray(img, mode="RGB").save('{}/{}.jpg'.format(params["output"], frame))
+        r_coeffs = map(lambda x: str(x), dct8(img_array_r)._compute_dct())
+        g_coeffs = map(lambda x: str(x), dct8(img_array_g)._compute_dct())
+        b_coeffs = map(lambda x: str(x), dct8(img_array_b)._compute_dct())
+
+        # write coeffs into a csv
+        # :::::format:::::
+        # frame_no, block_no, r_c1, r_c2...r_64, g_1...g_64, b_1...b64, segment
+        with open("{}/dct.csv".format(params["output"]), "a") as f:
+            for i in range(0, len(r_coeffs), 64):
+                row = '{},{},{},{},{},{}\n'.format(frame,
+                                                   i / 64,
+                                                   ','.join(r_coeffs[i: i + 64]),
+                                                   ','.join(g_coeffs[i: i + 64]),
+                                                   ','.join(b_coeffs[i: i + 64]),
+                                                   1)
+                f.write(row)
+            f.close()
+        frame += 1
 
 
 if __name__ == "__main__":
