9aab45b1ceac8ea1982d502a7ab6de996b8b1de1
Joel Mathew <joelmathew.mec@gmail.com>
2017-04-26 20:54:31 -0700

    Test output

--- DeadPixel/src/com/deadpixel/arbiter/Arbiter.java
+++ DeadPixel/src/com/deadpixel/arbiter/Arbiter.java
@@ -17,109 +17,36 @@ public class Arbiter {
 
 	public static void main(String[] args) {
 		try {
-			/*System.out.println("here");
-			try {// (SeekableByteChannel ch = Files.newByteChannel(Paths.get("/Users/fox/dev/workspace/out.bin"))) {
-				FileInputStream f = new FileInputStream( "/Users/fox/Documents/sem4/project/given/test.bin" );
-				FileChannel ch = f.getChannel( );
-			    ByteBuffer bb = ByteBuffer.allocateDirect(6364800);
-			    bb.order(ByteOrder.nativeOrder());
-			    for(;;) {
-			    	int n = ch.read(bb);
-			    	System.out.println(n);
-			    	float[] rBytes = new float[6364800/4];
-			    	
-			    	//bb.rewind();
-			        FloatBuffer fb = ((ByteBuffer) bb.rewind()).asFloatBuffer();
-			        fb.get(rBytes);
-			        System.out.println("Float Buffer");
-			        while (fb.hasRemaining())
-			          System.out.println(fb.position() + " -> " + fb.get());
-			          
-			    	for(int i=0,j=0;i<rBytes.length;i++,j+=4){
-			    		System.out.println(rBytes[i]);
-			            //ByteBuffer.wrap(bb, i*4, 4).putFloat(rBytes[i]);
-			            
-			        }
-			    	System.out.println("and here");
-			        
-			        // add chars to line
-			        // ...
-					System.exit(1);
-			    }
-			} catch(Exception e) {
-				e.printStackTrace();
-			}
-			System.exit(1);
-*/
-			//final long startTime = System.currentTimeMillis();
 			FramesUtil.ensureExistence();
-			/*Thread t1 = new Thread(new Decoder());
-			t1.start();
-			t1.join();
-			*/
 			FrameReader reader = new FrameReader();
 			reader.readBinary();
-			//System.out.println("Total time=" + (System.currentTimeMillis() - startTime));
-			JFrame frame = new JFrame();
-			GridBagLayout gLayout = new GridBagLayout();
-			frame.getContentPane().setLayout(gLayout);
-			
-			try {
-				System.out.println(FramesUtil.frameMap.size());
-				
-				JLabel lbIm1 = new JLabel(new ImageIcon(FramesUtil.frameMap.get(0).bufferedImage));//FramesUtil.imageQueue.take().bufferedImage));
-				GridBagConstraints c = new GridBagConstraints();
-				c.fill = GridBagConstraints.HORIZONTAL;
-				c.anchor = GridBagConstraints.CENTER;
-				c.fill = GridBagConstraints.HORIZONTAL;
-				c.gridx = 0;
-				c.gridy = 1;
-				frame.getContentPane().add(lbIm1, c);
-				frame.pack();
-				frame.setVisible(true);
-				//frame.addMouseMotionListener(this);
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			} catch(Exception e) {
-				e.printStackTrace();
-			}
+			testVideo(1);
+			testVideo(100);
+			testVideo(200);
+			testVideo(300);
+			testVideo(400);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	public void testVideo() {
-		FrameReader reader = new FrameReader();
-		//reader.readCSV();
-		
-		JFrame frame = new JFrame();
-		GridBagLayout gLayout = new GridBagLayout();
-		frame.getContentPane().setLayout(gLayout);
-		
+	public static void testVideo(int frameNum) {
 		try {
-			//System.out.println(FramesUtil.imageQueue.size());
-			
-			/*byte[] f = FramesUtil.imageQueue.take().frame;
-			System.out.println(f.length);
-			for(int i=0; i<64; i++) {
-				System.out.print(f[i]+", ");
-			}*/
-			
-			
-			
-			JLabel lbIm1 = new JLabel(new ImageIcon(FramesUtil.imageQueue.take().bufferedImage));
+			JLabel lbIm1 = new JLabel(new ImageIcon(FramesUtil.frameMap.get(frameNum).bufferedImage));//FramesUtil.imageQueue.take().bufferedImage));
 			GridBagConstraints c = new GridBagConstraints();
 			c.fill = GridBagConstraints.HORIZONTAL;
 			c.anchor = GridBagConstraints.CENTER;
 			c.fill = GridBagConstraints.HORIZONTAL;
 			c.gridx = 0;
 			c.gridy = 1;
+			JFrame frame = new JFrame();
+			GridBagLayout gLayout = new GridBagLayout();
+			frame.getContentPane().setLayout(gLayout);
 			frame.getContentPane().add(lbIm1, c);
 			frame.pack();
 			frame.setVisible(true);
-			//Thread.sleep(15000);
 			//frame.addMouseMotionListener(this);
-			//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
--- DeadPixel/src/com/deadpixel/digitize/FrameReader.java
+++ DeadPixel/src/com/deadpixel/digitize/FrameReader.java
@@ -1,12 +1,12 @@
 package com.deadpixel.digitize;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-import java.nio.FloatBuffer;
 import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,34 +21,48 @@ import com.univocity.parsers.conversions.Conversions;
 import com.univocity.parsers.csv.CsvParser;
 import com.univocity.parsers.csv.CsvParserSettings;
 
-public class FrameReader{
+public class FrameReader {
 
 	public void readBinary() {
 		try {
 			final long startTime = System.currentTimeMillis();
-			FileInputStream f = new FileInputStream("/Users/fox/Documents/sem4/project/given/test2");
+			File file = new File("/Users/fox/Documents/sem4/project/given/two_people_encoder_output.bin");
+			long fileLength = file.length();
+
+			FileInputStream f = new FileInputStream(
+					"/Users/fox/Documents/sem4/project/given/two_people_encoder_output.bin");
 			FileChannel ch = f.getChannel();
-			ByteBuffer bb = ByteBuffer.allocateDirect(6364800);
+
+			int frameLength = 6364800;
+			ByteBuffer bb = ByteBuffer.allocateDirect(frameLength);
 			bb.order(ByteOrder.nativeOrder());
-			
+
+			// System.out.println(fileLength/frameLength);
+			// System.exit(1);
+
 			ExecutorService exec = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-	        ArrayList<Callable<Boolean>> tasks = new ArrayList<Callable<Boolean>>();
-	        
-			for (int k=0;k<1;k++) {
+			ArrayList<Callable<Boolean>> tasks = new ArrayList<Callable<Boolean>>();
+
+			for (int k = 0; k < fileLength / frameLength; k++) {
 				int n = ch.read(bb);
 
-				FloatBuffer frameBuffer = ((ByteBuffer) bb.rewind()).asFloatBuffer();
+				// FloatBuffer frameBuffer = ((ByteBuffer)
+				// bb.rewind()).asFloatBuffer();
+
+				// for(Object input : inputs){
+				tasks.add(new Frame(((ByteBuffer) bb.rewind()).asFloatBuffer()));
+				// }
 				
-		        //for(Object input : inputs){
-		           tasks.add(new Frame(frameBuffer));
-		        //}
-		        List<Future<Boolean>> results = exec.invokeAll(tasks);
-		        exec.shutdown();
-		        for(Future f1 : results) {
-		           System.out.println(f1.get());
-		           System.out.println("Total time=" + (System.currentTimeMillis() - startTime));
-		        }
+				if((k!=0 && k%60 == 0) || (k == (fileLength / frameLength)-1 && (fileLength / frameLength)%60 != 0)) {
+					List<Future<Boolean>> results = exec.invokeAll(tasks);
+					for (Future f1 : results) {
+						//System.out.println("Time taken for " + (k+1) +" frames=" + (System.currentTimeMillis() - startTime));
+						tasks.clear();
+						break;
+					}
+				}
 			}
+			exec.shutdown();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -81,7 +95,7 @@ public class FrameReader{
 				// System.out.println(Arrays.toString(row));
 				// Frame frame = new Frame(row);
 				if ((double) row[1] == 0.0)
-					FramesUtil.frameMap.put((int)row[0], new Frame(row));
+					FramesUtil.frameMap.put((int) row[0], new Frame(row));
 				else if ((double) row[1] == 8159.0) {
 					Frame frame = FramesUtil.frameMap.get((double) row[0]);
 					frame.setBlock(row);
